3146ca03a7f1a4885ec192487b73493bac9b4954
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-09 10:05:58 +0700

    First Commit

---
+++ Latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Begin
+    // Numeric Luas, p, l
+    double Luas, p, l;
+    // display "Masukkan Panjangnya = "
+    cout << "Masukkan Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display "Masukkan Lebarnya = "
+    cout << "Masukkan Lebarnya = ";
+    // Accept l
+    cin >> l;
+    // Compute Luas = p*l
+    Luas = p*l;
+    // display "Luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    //End
+}
